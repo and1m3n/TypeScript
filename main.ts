e9b2946e932007020c8f60adce13368facf4e429
and1m3n <and1m3n@mail.ru>
2023-05-30 19:26:13 +0300

    примеры записи ф-й в TS

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-const a = 1;
+let revenue: number = 1000;
+let bonus: number = 500;
+
+let res: number = revenue + bonus;
+console.log(res);
+
+function getFullName(firstname: string, surname: string): string {
+  return `${firstname} ${surname}`;
+}
+
+const getFullNameArrow = (firstname: string, surname: string): string => {
+  return `${firstname} ${surname}`;
+};
